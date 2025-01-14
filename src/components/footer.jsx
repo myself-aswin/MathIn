07dea26d4e1054b384031e0faabb3085834cb298
Aswin V S <68040205+aswin-vs@users.noreply.github.com>
2025-01-14 21:44:32 +0530

    footerUpdate

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       Made with&nbsp;
       <img src={`${import.meta.env.BASE_URL}icons/heart.svg`} alt="heartIcon" />
       &nbsp;by&nbsp;
-      <a href="https://github.com/aswin-vs" target="_blank" rel="noreferrer">
+      <a href="https://github.com/myself-aswin" target="_blank" rel="noreferrer">
         <span>Aswin V S</span>
       </a>
     </footer>
